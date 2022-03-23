274b7b98564e32f46ac1f593698876d05b7a437e
Tyler Christiansen <code@tylerc.me>
2022-03-22 21:20:50 -0400

    Refactor to a new URI

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def root():
     num_requests = random.randint(0, 50)
     responses = []
     for _ in range(0, num_requests):
-        responses.append(random_number())
+        responses.append(requests.get(f"{EXAMPLE_SERVICE}/random/number").json())
 
     finish = datetime.now()
     duration = finish - start 
@@ -31,11 +31,13 @@ def root():
     }
 
 
+@app.route("/random/number")
 def random_number():
     # Simulate some problem in the app logic by sleeping for random time
-    # This will later become a new API endpoint that we break out
+    # This is a new API endpoint that we will rewrite in Golang later
     time.sleep(random.randint(0.0, 1.0))
     return {
+        "location": "/random/number",
         "number": random.randint(0, 9999)
     }
 
